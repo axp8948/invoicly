211068477acfc3b3c695f8c3d36d36f46b18421f
Anmol Pandey <axp8948@mavs.uta.edu>
2025-07-17 10:42:16 -0500

    Pages Basic Layout

--- src/components/HomePageComponent.jsx
+++ src/components/HomePageComponent.jsx
@@ -14,18 +14,18 @@ export default function Home() {
   return (
 <div className="flex flex-col items-center justify-center text-center px-4">
   <span className="text-5xl md:text-6xl font-extrabold text-white leading-tight mb-4">
-    Welcome to <span className="bg-gradient-to-r from-lime-300 to-green-400 text-transparent bg-clip-text">Invoicly</span>
+    Welcome to <span className="text-orange-700 text-transparent bg-clip-text">MavThread</span>
   </span>
 
   <ReactTyped
-    strings={['Track Invoices.', 'Manage Clients.', 'Grow Your Business.']}
+    strings={['By Mavericks', 'For Mavericks']}
     typeSpeed={50}
     backSpeed={30}
     loop
     className="text-lime-300 text-2xl font-medium mb-4"
   />
 
-  <p className="text-white text-lg mt-2 opacity-70">
+  <p className="text-white text-lg mt-16 opacity-70">
     The simplest way to organize your freelance business.
   </p>
 
